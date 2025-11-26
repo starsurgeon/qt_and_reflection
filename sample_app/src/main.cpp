1d4c605df6a53f206d29e37d68835390a009140b
Dr. Andreas Müller <andreas.mueller.ext@hacon.de>
2025-11-26 12:52:46 +0000

    devcontainer: add git feature and update customizations; create .dockerignore and update .gitignore; enhance CMakeLists.txt for compile commands export

--- sample_app/src/main.cpp
+++ sample_app/src/main.cpp
@@ -5,5 +5,11 @@ int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
     qDebug() << "Qt version:" << qVersion();
+    std::vector< int > data;
+    data.push_back(2);
+    data.push_back(3);
+    for (const auto &value : data) {
+        qDebug() << "Value:" << value;
+    }
     return 0;
 }
